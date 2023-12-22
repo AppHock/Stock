48c21e43dabcc14f4ffb15c13ce11faa8d91764a
chengpeng <peng.cheng@pixocial.com>
2023-12-22 20:39:30 +0800

    完成每日代码池逻辑

--- 每日代码池.py
+++ 每日代码池.py
@@ -1,48 +1,76 @@
 import tushare as tu
 import time
 import datetime
+import platform
+import sys, os
 
 token = '47aca0f52e01163f8fae34938cad4b776021ff2cc1678e557b744899' # 阿文的token
 tu.set_token(token)
 pro = tu.pro_api()
 
+globalSys_Mac = platform.system().lower() == 'darwin'
+print('当前操作系统:%s' % platform.system().lower())
+
+
 # 当前日期的 Unix 时间
 g_currentDateUnix = 0
 g_currentDate = ''
 
+# 逻辑：路径转换
+def pathToSys(path):
+    if globalSys_Mac:
+        return path
+    else:
+        return path.replace('/', '\\')
+    
+globalPath = pathToSys(os.getcwd() + '/')
+if not globalSys_Mac:
+    globalPath = 'C:\\Users\\Administrator\\Desktop\\股票数据\\'
+
+# 获得前一天的时间
+def getPreDateAndUnixTime(dateUnixTime):
+    currentUnixTime = dateUnixTime - 60 * 60 * 24 * 1
+    return [unixTime2LocalDate(currentUnixTime, "%Y%m%d"), currentUnixTime]
+
+# 获得后一天的时间
+def getBackDateAndUnixTime(dateUnixTime):
+    currentUnixTime = dateUnixTime + 60 * 60 * 24 * 1
+    return [unixTime2LocalDate(currentUnixTime, "%Y%m%d"), currentUnixTime]
+
 # date字符串转Unix时间
-def date2UnixTime(dt):
+def date2UnixTime(dt, format="%Y%m%d"):
     #转换成时间数组 %Y-%m-%d %H:%M:%S
-    timeArray = time.strptime(dt, "%Y%m%d")
+    timeArray = time.strptime(dt, format)
     #转换成时间戳
     timestamp = time.mktime(timeArray)
     return timestamp
 
 # Unix时间转date字符串
-def unixTime2LocalDate(timestamp, dateFormat="%Y-%m-%d %H:%M:%S"):
+def unixTime2LocalDate(timestamp, dateFormat="%Y%m%d"):
     #转换成localtime
     time_local = time.localtime(timestamp)
     #转换成新的时间格式(2016-05-05 20:28:54)
     dt = time.strftime(dateFormat, time_local)
     return dt
 
-def getCurrentDateUnixTime(format="%Y%m%d"):
+def getCurrentDate(format="%Y%m%d"):
     return datetime.datetime.today().strftime(format)
+
+def getCurrentDateUnixTime(format="%Y%m%d"):
+    return date2UnixTime(getCurrentDate())
 
 # 判断某个日期是否满一年
 def beyondOneYear(date):
     global g_currentDateUnix, g_currentDate
     if g_currentDateUnix == 0:
-        curDate = getCurrentDateUnixTime()
-        g_currentDate = curDate
-        curDateUnix = date2UnixTime(curDate)
-        g_currentDateUnix = curDateUnix
+        g_currentDate = getCurrentDate()
+        g_currentDateUnix = getCurrentDateUnixTime()
     dateUnix = date2UnixTime(date)
     return g_currentDateUnix - dateUnix >= 31536000
 
 
 
-# 当前正常上市交易的股票，指定交易所(SSE上交所 SZSE深交所)
+# 当前正常上市交易的股票，指定交易所(SSE上交所 SZSE深交所)，要求上市满一年，剔除 ST 和退市
 def getCurrentNormal(exchange):
     stokeCodes = []
     stokeNames = []
@@ -55,9 +83,6 @@ def getCurrentNormal(exchange):
         name = codeInfo[2]
         
         fisrtDate = codeInfo[3]
-
-        if '20230510' == fisrtDate:
-            print('ww')
         # 上市未满一年
         if not beyondOneYear(fisrtDate):
             continue
@@ -70,13 +95,25 @@ def getCurrentNormal(exchange):
         stokeDates.append(fisrtDate)
     return stokeCodes
 
-# 前 5 日未触及涨停板和停牌
+# 前 5 交易日，未触及停牌
+def suspended(day = 5):
+    datas = pro.query('suspend', ts_code='', suspend_date='20231218', resume_date='', fields='')
+    codes = []
+    for data in datas.values:
+        codes.append(data[0])
+    return codes
 
 # 是否触及涨停
-def isZhang(code, chage, tradeDate):
-    if (('300' in code) | ('688' in code)) & (chage > 0.198):
-        print('出现了涨停，代码是:%s, 涨幅:%f, 日期是:%s' % (code, chage, tradeDate))
-        return True
+def isZhang(code, tradeDate, high, preClose):
+    chage = (high-preClose)/preClose
+    if (('30' in code[:2]) | ('688' in code[0:3])):
+        if chage > 0.198:
+            print('出现了涨停，代码是:%s, 涨幅:%f, 日期是:%s' % (code, chage, tradeDate))
+            return True
+        return False
+    else:
+        if chage > 0.098:
+            print('出现了涨停，代码是:%s, 涨幅:%f, 日期是:%s' % (code, chage, tradeDate))
     return chage > 0.098
 
 
@@ -91,14 +128,13 @@ def getDayKLine(codes, day):
     todayClose = 0
     allClose = 0
     times = 0
-    print('正在处理的code：%s'%codes)
     for data in datas.values:
         code = data[0]
-        trade_date = data[1]
         high = data[2]
         pre_close = data[3]
-        high_chg = (high-pre_close)/pre_close
-        return isZhang(code, high_chg, trade_date)
+        trade_date = data[1]
+        if isZhang(code, trade_date, high, pre_close):
+            return True
     return False
 
 # 逻辑：把数组里面的数据组成字符串
@@ -112,22 +148,87 @@ def getStrWithList(list):
     return str
 
 # 通过日期获取日K 数据
-def getDaykLine(date):
-    df = pro.daily(trade_date='20180810')
+def getDaykLine(day):
+    origin = day
+    curDate = getCurrentDate()
+    
+    date = curDate
+    dateUnix = date2UnixTime(date)
+    allCodeInfos = {}
+    while(day):
+        datas = pro.daily(trade_date=date, fields='ts_code, trade_date, high, pre_close')
+        if len(datas.values) != 0:
+            for data in datas.values:
+                code = data[0]
+                codeInfos = allCodeInfos.get(code, [])
+                codeInfos.append(data)
+                allCodeInfos[code] = codeInfos
+            day -= 1
+        dateUnix = getPreDateAndUnixTime(dateUnix)[1]
+        date = unixTime2LocalDate(dateUnix)
+    print('最近%d天 日K 数据获取完成' % origin)
+    return allCodeInfos
 
 # 获取最近几个交易日的数据
 # def getRecentlyDayK(day):
     
+# 逻辑：往本地重写数据
+def writeCodesToLocalExcel(codes, isSZ):
+    global globalPath
+    suffix = "上证"
+    if isSZ:
+        suffix = "深证"
+    saveDir = pathToSys(globalPath + '每日代码池/')
+    if not os.path.exists(saveDir):
+        os.makedirs(saveDir)
     
+    path = saveDir + suffix + '.csv'
+    symbolFile = open(path, 'w+', encoding='utf8')
+    # 写入头部名称
+    symbolFile.write('股票代码\n')
+    for code in codes:
+        s = '%s\n' % (code)
+        symbolFile.write(s)
+    symbolFile.flush()
+    print(suffix + '的交易数据保存 Excel 完成')
 
-
-if __name__ == "__main__":
-
-    codes = getCurrentNormal('SSE')
-    codes_str = getStrWithList(codes)
+# 剔除触及涨停的股
+def removeTouchZhang(allCodeInfos, codes):
     answer = []
     for code in codes:
-        if getDayKLine(code,7):
+        canAdd = True
+        for codeInfo in allCodeInfos[code]:
+            code = codeInfo[0]
+            trade_date = codeInfo[1]
+            high = codeInfo[2]
+            pre_close = codeInfo[3]
+            if isZhang(code, trade_date, high, pre_close):
+                canAdd = False
+                break
+        if canAdd:
             answer.append(code)
+    return answer
+
+if __name__ == "__main__":
+    # 获取上交所
+    codes_sh = getCurrentNormal('SSE')
+    codes_sz = getCurrentNormal('SZSE')
+
+    # 移除最近 5 日停盘的股
+    suspendedCodes = suspended()
+    for susCode in suspendedCodes:
+        if susCode in codes_sh:
+            codes_sh.remove(susCode)
+        
+        if susCode in codes_sz:
+            codes_sz.remove(susCode)
+
+    allCodeInfos = getDaykLine(5)
+
+    # 获取最近 5 天的日 K 数据
+    answer_sh = removeTouchZhang(allCodeInfos, codes_sh)
+    answer_sz = removeTouchZhang(allCodeInfos, codes_sz)
     
-    print('需要创建的文件路径')
+    writeCodesToLocalExcel(answer_sh, False)
+    writeCodesToLocalExcel(answer_sz, True)
+    print('每日代码池更新完成')
